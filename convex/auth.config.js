533fbe8553129b55d07de3f8ec9dc20a8dec15b6
Muhammad Sajjad <msajjad87@icloud.com>
2026-02-08 13:42:26 +0400

    new update

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -4,5 +4,9 @@ export default {
             domain: "https://clever-polliwog-8.clerk.accounts.dev",
             applicationID: "convex",
         },
+        {
+            domain: "https://clerk.dtraders.in",
+            applicationID: "convex",
+        },
     ],
 };
